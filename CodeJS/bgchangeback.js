29e112417806972056a50d9da96c89f0c54f6e1a
Vexify4 <26hanleylj@gmail.com>
2021-10-14 14:20:59 -0700

    Update bgchangeback.js

--- CodeJS/bgchangeback.js
+++ CodeJS/bgchangeback.js
@@ -1,1 +1,3 @@
-javascript:var newImage = prompt("URL Image", "url('https://i.ytimg.com/vi/Lr5E2KBfEGE/maxresdefault.jpg')");var wae=document.body;wae.style.backgroundImage=newImage,wae.style.backgroundRepeat="no-repeat",wae.style.backgroundAttachment="fixed";void 0;
+javascript:
+var newImage = prompt("URL Image", "url('https://i.ytimg.com/vi/Lr5E2KBfEGE/maxresdefault.jpg')");
+var wae=document.body;wae.style.backgroundImage=newImage,wae.style.backgroundRepeat="no-repeat",wae.style.backgroundAttachment="fixed";void 0;
